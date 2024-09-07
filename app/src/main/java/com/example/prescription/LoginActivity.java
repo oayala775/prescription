4a3745ba5c6d89902c3db662ba38d2c604ba3bc6
Omar Ayala <oayala775@gmail.com>
2024-09-07 00:04:47 -0600

    Added functionality to login buttons and created styles

--- app/src/main/java/com/example/prescription/LoginActivity.java
+++ app/src/main/java/com/example/prescription/LoginActivity.java
@@ -1,17 +1,29 @@
 package com.example.prescription;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    public Button registerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_signup_usuario);
+        setContentView(R.layout.activity_login);
 
+        registerButton = findViewById(R.id.button3);
+        registerButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/prescription/SignUpActivity.java
+++ app/src/main/java/com/example/prescription/SignUpActivity.java
@@ -1,6 +1,9 @@
 package com.example.prescription;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +11,50 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    public Button signUpUser;
+    public Button signUpDoc;
+    public Button signUpPharm;
+    public Button signIn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_signup);
+
+        signUpUser = findViewById(R.id.buttonUser);
+        signUpDoc = findViewById(R.id.buttonDoctor);
+        signUpPharm = findViewById(R.id.buttonPharm);
+        signIn = findViewById(R.id.buttonSignIn);
+
+        signUpUser.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(SignUpActivity.this, SignUpUsuarioActivity.class);
+                startActivity(intent);
+            }
+        });
+        signUpDoc.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(SignUpActivity.this, SignUpUsuarioActivity.class);
+                startActivity(intent);
+            }
+        });
+        signUpPharm.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(SignUpActivity.this, SignUpUsuarioActivity.class);
+                startActivity(intent);
+            }
+        });
+        signIn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
