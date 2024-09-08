185fe2ef6b333a67c517d68443f45b82a09ebead
RubenLimon18 <ruben.limonrangel@gmail.com>
2024-09-08 16:06:11 -0600

    Added function back button in activity_signup_usuario

--- app/src/main/java/com/example/prescription/SignUpUsuarioActivity.java
+++ app/src/main/java/com/example/prescription/SignUpUsuarioActivity.java
@@ -1,6 +1,8 @@
 package com.example.prescription;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +10,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUpUsuarioActivity extends AppCompatActivity {
 
+    public ImageView btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_signup_usuario);
+
+        btnBack = (ImageView) findViewById(R.id.flecha_izq);
+        btnBack.setOnClickListener(v -> {
+            Intent intent = new Intent(SignUpUsuarioActivity.this, SignUpActivity.class);
+            startActivity(intent);
+        });
     }
 }
