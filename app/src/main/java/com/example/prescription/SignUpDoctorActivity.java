c93d6388d9488990dd8a1994255b258abcf4a084
Mile <dixieac2593@gmail.com>
2024-10-18 13:41:48 -0600

    base de datos doctor (guardar)

--- app/src/main/java/com/example/prescription/SignUpDoctorActivity.java
+++ app/src/main/java/com/example/prescription/SignUpDoctorActivity.java
@@ -3,17 +3,56 @@ package com.example.prescription;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUpDoctorActivity extends AppCompatActivity {
 
+    public EditText nombre_p, apellido_p, telefono_p, nss_p, curp_p, fecha_nacimiento_p, domicilio_p, ciudad_p, colonia_p;
+    public TextView cedula_p;
+    public Button guardar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_signup_doctor);
+
+        nombre_p = (EditText) findViewById(R.id.doctor_nombre);
+        apellido_p = (EditText) findViewById(R.id.doctor_apellido);
+        telefono_p = (EditText) findViewById(R.id.doctor_telefono);
+        nss_p = (EditText) findViewById(R.id.doctor_nss);
+        curp_p = (EditText) findViewById(R.id.doctor_curp);
+        //fecha_nacimiento = findViewById(R.id.doctor_fechaNacimiento);
+        domicilio_p = (EditText) findViewById(R.id.doctor_domicilio);
+        colonia_p = (EditText) findViewById(R.id.doctor_colonia);
+        ciudad_p = (EditText) findViewById(R.id.doctor_ciudad);
+        cedula_p = (EditText) findViewById(R.id.doctor_cedula);
+        guardar = (Button) findViewById(R.id.doctor_guardar);
+
+        guardar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                DB db = new DB(getApplicationContext(), null, null, 1);
+                String nombre = nombre_p.getText().toString();
+                String apellido = apellido_p.getText().toString();
+                String telefono = telefono_p.getText().toString();
+                String nss = nss_p.getText().toString();
+                String curp = curp_p.getText().toString();
+                String domicilio = domicilio_p.getText().toString();
+                String colonia = colonia_p.getText().toString();
+                String ciudad = ciudad_p.getText().toString();
+                String cedula = cedula_p.getText().toString();
+                String mensaje = db.guardar(nombre,apellido,telefono, nss, curp, domicilio, ciudad, colonia, cedula);
+                Toast.makeText(getApplicationContext(), mensaje, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
 
