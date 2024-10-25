934911af84a05001228d6b808b2bfa032da8c23c
RubenLimon18 <ruben.limonrangel@gmail.com>
2024-10-25 16:53:58 -0600

    Dinamyc farmacia profile information

--- app/src/main/java/com/example/prescription/PerfilFarmaciaActivity.java
+++ app/src/main/java/com/example/prescription/PerfilFarmaciaActivity.java
@@ -1,5 +1,6 @@
 package com.example.prescription;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -9,11 +10,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 
 public class PerfilFarmaciaActivity extends AppCompatActivity {
     public EditText nombre, telefono, domicilio, colonia, ciudad;
     public TextView codigo;
+    private ArrayList<String> informacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +24,25 @@ public class PerfilFarmaciaActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_perfil_farmacia);
 
-        String nombre_parametro = "Farmacia Patito";
-        long telefono_parametro = 9999999999L;
-        String domicilio_parametro = "Calle 1 numero #1";
-        String colonia_parametro = "Colonia 1";
-        String ciudad_parametro = "Ciudad";
-        long codigo_parametro = 21212121212L;
+        //Obtenemos informacion del intent
+        Intent intentInformacion = getIntent();
+        informacion = intentInformacion.getStringArrayListExtra("datos_farmacia");
+
+
+        //String nombre_parametro = "Farmacia Patito";
+        //long telefono_parametro = 9999999999L;
+        //String domicilio_parametro = "Calle 1 numero #1";
+        //String colonia_parametro = "Colonia 1";
+        //String ciudad_parametro = "Ciudad";
+        //long codigo_parametro = 21212121212L;
+
+
+        String nombre_parametro = informacion.get(1);
+        String telefono_parametro = informacion.get(2);
+        String domicilio_parametro = informacion.get(3);
+        String ciudad_parametro = informacion.get(4);
+        String colonia_parametro = informacion.get(5);
+        String codigo_parametro = informacion.get(0);
 
         codigo = findViewById(R.id.farmacia_codigo);
         nombre = findViewById(R.id.farmacia_nombre);
